78e40000e2429b5a5b4f64500abb987ac45070d5
wtsclwq <lwqcode@foxmail.com>
2023-07-26 16:00:33 +0800

    update: string

--- str.cpp
+++ str.cpp
@@ -6,44 +6,63 @@
 class Str{
 public:
     // default constructor
-    Str() : length_(0), capacity_(DEFAULT_CAPACITY){
+    Str():length_(0), capacity_(DEFAULT_CAPACITY){
+        std::cout << "[default] constructor" << std::endl;
         raw_str_ = new char[capacity_]{};
     }
 
     // constructor for const char *
-     explicit Str(const char *str):length_(std::strlen(str)),capacity_(std::max(DEFAULT_CAPACITY,length_+1)){
-        std::cout << "constructor for const char *: " << str <<  std::endl;
+    explicit Str(const char *str){
+        std::cout << "[arg] constructor for const char *" << std::endl;
+        length_ = std::strlen(str);  // do not count '\0'
+        capacity_ = std::max(length_ + 1, DEFAULT_CAPACITY); // need to save '\0'
         raw_str_ = new char[capacity_]{};
         std::strcpy(raw_str_, str);
     }
 
     // copy constructor
-    Str(const Str &other):length_(other.length_),capacity_(other.capacity_){
-        std::cout << "copy constructor: " << other.raw_str_ << std::endl;
-        raw_str_ = new char[capacity_];
+    Str(const  Str& other):length_(other.length_),capacity_(other.capacity_){
+        std::cout << "[copy] constructor: other.raw_str_ = " << other.raw_str_ << std::endl;
+        raw_str_ = new char[capacity_]{};
         std::strcpy(raw_str_, other.raw_str_);
     }
 
     // move constructor
-    Str(Str &&other) noexcept: raw_str_(other.raw_str_),length_(other.length_),capacity_(other.capacity_){
-        std::cout << "move constructor: " << other.raw_str_ << std::endl;
+    Str(Str &&other) noexcept:length_(other.length_),capacity_(other.capacity_),raw_str_(other.raw_str_){
+        std::cout << "[move] constructor: other.raw_str_ = " << other.raw_str_ << std::endl;
         other.length_ = 0;
         other.capacity_ = 0;
         other.raw_str_ = nullptr;
     }
 
-    auto operator=(const char *str)-> Str&{
-        std::cout << "const char * assign operator" << std::endl;
-        Str temp{str};
+    // assign operator for const char *
+    auto operator=(const char *str) -> Str&{
+        std::cout << "assign operator for const char *" << std::endl;
+        auto temp = Str(str);
         temp.swap(*this);
         return *this;
     }
 
-    // copy and swap
-    auto operator=(Str other)-> Str&{
-        std::cout << "copy and swap assign operator" << std::endl;
+    // assign operator for both copy and move
+    auto operator=(Str other) -> Str&{
+        std::cout << "copy and swap assign operator for both copy and move" << std::endl;
         other.swap(*this);
         return *this;
+    }
+
+    ~Str(){
+        delete[] raw_str_;
+    }
+
+    void reserve(size_t cap){
+        if(cap > capacity_){
+            auto new_str = new char[capacity_]{};
+            std::strcpy(new_str, raw_str_);
+            delete[] raw_str_;
+
+            capacity_ = cap;
+            raw_str_ = new_str;
+        }
     }
 
     void swap(Str &other){
@@ -53,50 +72,8 @@ public:
         swap(other.raw_str_, raw_str_);
     }
 
-    void reserve(size_t new_cap){
-        if (new_cap > capacity_){
-            char *new_raw_str = new char[new_cap]{};
-            std::strcpy(new_raw_str, raw_str_);
-            delete[] raw_str_;
-            raw_str_ = new_raw_str;
-            capacity_ = new_cap;
-        }
-    }
-
-    void resize(size_t new_size){
-        if (new_size < length_) {
-            length_ = new_size;
-            raw_str_[length_] = '\0';
-        } else if (new_size > length_ && new_size <= capacity_){
-            for (size_t i = length_; i < new_size; ++i) {
-                raw_str_[i] = '\0';
-            }
-            length_ = new_size;
-            raw_str_[length_] = '\0';
-        }else{
-            reserve(new_size);
-            length_ = new_size;
-        }
-    }
-
-    auto c_str() -> const char* {
-        return raw_str_;
-    }
-
-    auto data() ->char*{
-        return raw_str_;
-    }
-
-    auto length() const -> size_t {
-        return length_;
-    }
-
-    auto operator[](size_t index) -> char&{
-        return raw_str_[index];
-    }
-
     void push_back(char ch){
-        if(length_ + 1 >= capacity_){
+        if (length_ + 1 >= capacity_) {
             reserve(capacity_ * 2);
         }
         raw_str_[length_] = ch;
@@ -104,11 +81,28 @@ public:
         length_++;
     }
 
-    void pop_back() {
+    void pop_back(){
         if (length_ > 0) {
-            raw_str_[length_ - 1] = '\0';
+            raw_str_[length_-1] = '\0';
             length_--;
         }
+    }
+
+    auto data() -> char* {
+        return raw_str_;
+    }
+
+    auto c_str() const -> const char* {
+        return raw_str_;
+    }
+
+    auto operator[] (int index)-> char&{
+        return raw_str_[index];
+    }
+
+    friend auto operator<<(std::ostream& os, const Str& str) -> std::ostream&{
+        os << str.c_str();
+        return os;
     }
 
 private:
@@ -119,22 +113,22 @@ private:
 };
 
 int main(){
-    Str sss{"init"};
+    Str sss{};
     std::cout << "\n";
 
     sss = "xxxxx";
-    std::cout << "current sss=" << sss.data() <<  "\n\n";
+    std::cout << "current sss=" << sss <<  "\n\n";
 
     Str str1{"str1"};
     sss = str1;
-    std::cout << "current sss=" << sss.data() <<  "\n\n";
+    std::cout << "current sss=" << sss <<  "\n\n";
 
     Str str2{"str2"};
     sss = std::move(str2);
-    std::cout << "current sss=" << sss.data() <<  std::endl;
+    std::cout << "current sss=" << sss <<  std::endl;
 
     sss.push_back('a');
-    std::cout << sss.data() << std::endl;
+    std::cout << sss << std::endl;
     sss.pop_back();
-    std::cout << sss.data();
+    std::cout << sss;
 }
